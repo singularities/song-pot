c1b4d59dc6d4e96b2393ab77d40d0bfee03d0f2e
Antonio Tapiador del Dujo <atd@singularities.org>
2016-07-18 12:20:15 +0200

    Android support

--- client/main.js
+++ client/main.js
@@ -6,3 +6,13 @@ angular.module('songlog', [
   angularMeteor,
   songList.name
 ]);
+
+function onReady () {
+  angular.bootstrap(document, [ 'songlog' ]);
+}
+
+if (Meteor.isCordova) {
+  angular.element(document).on('deviceready', onReady);
+} else {
+  angular.element(document).ready(onReady);
+}
