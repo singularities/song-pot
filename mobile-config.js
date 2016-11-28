09a6a29c7df71a016a88530238571d618df3e26d
Antonio Tapiador del Dujo <atd@singularities.org>
2016-11-28 13:14:48 +0100

    Android app info and icons

---
+++ mobile-config.js
@@ -0,0 +1,13 @@
+App.info({
+  id: 'org.singularities.songspot',
+  name: 'Songs Pot',
+  version: "0.0.1"
+});
+
+App.icons({
+  'android_mdpi': 'icons/icon-48.png', // (48x48)
+  'android_hdpi':   'icons/icon-72.png', // (72x72)
+  'android_xhdpi':  'icons/icon-96.png', // (96x96)
+  'android_xxhdpi': 'icons/icon-144.png', // (144x144)
+  'android_xxxhdpi': 'icons/icon-192.png' // (192x192)
+});
