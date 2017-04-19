f1e9966c0a8d5023fbae05c42e548659e6082848
Antonio Tapiador del Dujo <atd@singularities.org>
2017-04-19 10:41:41 +0200

    Clean unused import

--- client/imports/audio/list.component.ts
+++ client/imports/audio/list.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { MdDialog, MdDialogRef } from '@angular/material';
+import { MdDialog } from '@angular/material';
 
 import { Audio } from '../../../imports/models';
 import { Audios } from '../../../imports/collections';
