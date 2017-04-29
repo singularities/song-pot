7be0012c22dee049dfb6a012e4d06e9b9170eb22
Antonio Tapiador del Dujo <atd@singularities.org>
2017-04-29 12:13:02 +0200

    Find and show band name

--- client/imports/band/band.component.ts
+++ client/imports/band/band.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+
+import { Bands } from '../../../imports/collections';
+import { Band } from '../../../imports/models';
 
 import template from "./band.html";
 
@@ -8,4 +12,21 @@ import template from "./band.html";
 })
 
 export class BandComponent {
+
+  band: Band;
+
+  constructor(private ngZone: NgZone,
+              private route: ActivatedRoute) {}
+
+  ngOnInit() {
+
+    this.route.params
+      .filter((params: Params) => ! this.band || this.band._id !== params['id'])
+      .switchMap((params: Params) => Bands.find({ _id: params['id']}))
+      .subscribe(bands => {
+        this.ngZone.run(() => {
+          this.band = bands[0];
+        });
+      });
+  }
 }
