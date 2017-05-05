69ce3b9be3c49b2e5001e40f92f93c0dd5662047
Antonio Tapiador del Dujo <atd@singularities.org>
2017-05-05 13:51:19 +0200

    Remove obsolete code

--- client/imports/song/edit.component.ts
+++ client/imports/song/edit.component.ts
@@ -13,28 +13,3 @@ export class SongEditComponent {
   @Input() song: Song;
 
 }
-
-/*
-  confirmRemove (ev) {
-    var namespace = 'song.remove.dialog.';
-    var translateKeys = [ 'cancel', 'confirm', 'message' ].map((t) => {
-      return namespace + t;
-    });
-
-    this.$translate(translateKeys, { 'title': this.song.title }).then((translations) => {
-
-      var confirm = this.$mdDialog.confirm()
-        .targetEvent(ev)
-        .title(translations[namespace + 'message'])
-        .ok(translations[namespace + 'confirm'])
-        .cancel(translations[namespace + 'cancel']);
-
-      this.$mdDialog.show(confirm).then(() => {
-        Songs.remove(this.song._id);
-
-        this.$state.go('songs');
-      });
-    });
-  }
-}
-  */
