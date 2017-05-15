430ed2ce17be23a12d8600d167f416d5222c9a09
Antonio Tapiador del Dujo <atd@singularities.org>
2017-05-15 13:48:39 +0200

    Ensure tests are run on desktop

--- tests/e2e.js
+++ tests/e2e.js
@@ -6,6 +6,12 @@ exports.config = {
   directConnect: true,
   specs: [ 'e2e/**/*.js' ],
   baseUrl: 'http://localhost:3000',
+  capabilities: {
+    browserName: 'chrome',
+    chromeOptions: {
+      args: ['--window-size=1024,800']
+    }
+  },
   onPrepare: function () {
     var registerPage = new sessionPages.Register(),
         frontPage  = new FrontPage(),
