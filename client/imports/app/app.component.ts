6ba6238427ac92597f28fce1cb4f98378b8bdebc
Antonio Tapiador del Dujo <atd@singularities.org>
2017-05-26 17:27:28 +0200

    Use angular router to redirect to intent urls
    It is faster than reloading the window
    
    Also, preserve query parameters, needed for disourse sso

--- client/imports/app/app.component.ts
+++ client/imports/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { Angulartics2GoogleAnalytics } from 'angulartics2';
+import { Meteor } from 'meteor/meteor';
 
 import { AudioPlayService } from '../audio/play.service';
 import { AudioUploadService } from '../audio/upload.service';
@@ -17,12 +19,25 @@ import template from "./app.html";
 })
 
 export class SongPot {
-  constructor(translate: TranslateService,
+  constructor(private router: Router,
+              translate: TranslateService,
               angulartics2GoogleAnalytics: Angulartics2GoogleAnalytics) {
     // this language will be used as a fallback when a translation isn't found in the current language
     translate.setDefaultLang('en');
 
     // use user's language
     translate.use(translate.getBrowserLang());
+  }
+
+  ngOnInit() {
+
+    if (Meteor.isCordova) {
+      let pathRegexp = /.+?\:\/\/.+?(\/.*)$/;
+
+      universalLinks.subscribe(null, (event) => {
+
+        this.router.navigateByUrl(pathRegexp.exec(event.url)[1]);
+      });
+    }
   }
 }
--- client/main.ts
+++ client/main.ts
@@ -7,10 +7,4 @@ import { AppModule } from './imports/app/app.module';
 
 Meteor.startup(() => {
   platformBrowserDynamic().bootstrapModule(AppModule);
-
-  if (Meteor.isCordova) {
-    universalLinks.subscribe(null, function (eventData) {
-      window.location.pathname = eventData.path;
-    });
-  }
 });
