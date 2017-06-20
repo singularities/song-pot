cadfb71331e13ca400feed9085d8544500e121f4
Antonio Tapiador del Dujo <atd@singularities.org>
2017-06-20 19:32:45 +0200

    Import Bands in fixtures

--- tests/e2e/fixtures/index.js
+++ tests/e2e/fixtures/index.js
@@ -20,20 +20,25 @@ class Fixtures {
 
   prepare() {
 
-
     return meteorPage.exec(`
       Accounts.findUserByEmail("${ this.user.email}") ||
       Accounts.createUser(${ JSON.stringify(this.user)})
-    `).then(() => {
-      return meteorPage.exec(`
-        Bands.collection.insert({
-          name: "${ this.band.name }",
-          createdAt: new Date(),
-          userIds: [],
-          songIds: []
-        })
-      `).then((bandId) => global.fixtures.band.id = /"(.*)"/.exec(bandId)[1]);
-    });
+    `)
+    // This commnad hacks the STDIN restriction
+    // that wraps commands in parenthesis
+    // https://github.com/meteor/meteor/issues/8823
+    .then(() => meteorPage.exec(`
+      "");
+      import { Bands } from "./imports/collections";
+      (
+      Bands.collection.insert({
+        name: "${ this.band.name }",
+        createdAt: new Date(),
+        userIds: [],
+        songIds: []
+      })
+    `))
+    .then((bandId) => global.fixtures.band.id = /"(.*)"/.exec(bandId)[1]);
   }
 }
 
