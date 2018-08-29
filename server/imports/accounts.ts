ebee5df48129f02d14e54979b13ac6f2b3a2e069
Antonio Tapiador del Dujo <atd@singularities.org>
2018-08-29 18:59:13 +0200

    Disable login expiration

--- server/imports/accounts.ts
+++ server/imports/accounts.ts
@@ -1,5 +1,9 @@
 import { Accounts } from 'meteor/accounts-base';
 
+Accounts.config({
+    loginExpirationInDays: null
+});
+
 Accounts.emailTemplates.siteName = 'Song Pot';
 Accounts.emailTemplates.from = 'Song Pot Admin <no-reply@songpot.rocks>';
 
